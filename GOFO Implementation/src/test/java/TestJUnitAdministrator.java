a576d5d09dc2616943b5e3f94b40f170f308f537
Vinicius Cayres <56272453+cayres2000@users.noreply.github.com>
2023-11-17 15:25:04 -0300

    Update TestJUnitAdministrator.java

--- GOFO Implementation/src/test/java/TestJUnitAdministrator.java
+++ GOFO Implementation/src/test/java/TestJUnitAdministrator.java
@@ -1,1 +1,12 @@
+package System;
 
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class TestJUnitAdministrator
+{
+  
+}
