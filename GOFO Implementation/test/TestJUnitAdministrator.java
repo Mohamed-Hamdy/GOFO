a5ab15eef63c4a353d2f08b9c5beb4d2297a3bc4
Vinicius Cayres <56272453+cayres2000@users.noreply.github.com>
2023-11-17 17:55:08 -0300

    Add files via upload

---
+++ GOFO Implementation/test/TestJUnitAdministrator.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.Test;
+import org.junit.Before;
+import UI.SystemUI;
+import System.Administrator;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class TestJUnitAdministrator
+{
+    Administrator adm;
+    @Before
+    public void testeCriandoAdm()
+    {
+        adm = new Administrator();
+    }
+
+    @Test
+    public void testeRetornoLoginSenhaDefault()
+    {   
+        String email = adm.getEmail();
+        String senha = adm.getPasswrod();
+
+        assertEquals("admin@gmail.com",email);
+        assertEquals("123",senha);
+    }
+
+    //@Test
+    //public void teste
+}
