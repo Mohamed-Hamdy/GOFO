1f73760b0a9c94fcf244cf6c3d10efa6ba7e4451
Vinicius Cayres <56272453+cayres2000@users.noreply.github.com>
2023-11-20 15:33:04 -0300

    Add files via upload

---
+++ GOFO Implementation/test/TestJUnitPlayer.java
@@ -0,0 +1,50 @@
+package test;
+
+import org.junit.Test;
+import org.junit.Before;
+import System.Player;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class TesJUnitPlayer {
+    Player player;
+    InputStream sysInBackup = System.in; // backup System.in to restore it later
+    ByteArrayInputStream in;
+    
+    @Before
+    public void criandoPlayer()
+    {
+        player = new Player();
+        player.setFName("Rogerio");
+        player.setLName("Silva");
+        player.setID(1);
+        player.setPassword("1234");
+        player.setEmail("rogerio@email.com");
+        player.setPhone(40028922);
+        player.setLocation("Rua Teste");
+        player.setRule("Regra");
+        player.setBalance(1000);
+    }
+
+    @Test
+    public void testeVerificaDadosPlayer()
+    {
+        assertEquals("Rogerio Silva",player.getFullName());
+        assertEquals(1,player.getID());
+        assertEquals("1234",player.getPassword());
+        assertEquals("rogerio@email.com",player.getEmail());
+        assertEquals(40028922,player.getPhone());
+        assertEquals("Regra",player.getRule());
+        assertEquals(1000,player.getBalance());
+        //Não existe um getLocation
+    }
+
+    @Test
+    public void testePlayerEscrevendoBalance()
+    {
+        in = new ByteArrayInputStream("2000".getBytes());
+        System.setIn(in);
+        player.setBalance();
+        assertEquals(2000,player.getBalance());
+    }
+}
