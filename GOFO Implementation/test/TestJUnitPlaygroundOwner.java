49093af5421f16577512e75332bbc86045c9c74d
Vinicius Cayres <56272453+cayres2000@users.noreply.github.com>
2023-11-21 02:15:51 -0300

    Update TestJUnitPlaygroundOwner.java

--- GOFO Implementation/test/TestJUnitPlaygroundOwner.java
+++ GOFO Implementation/test/TestJUnitPlaygroundOwner.java
@@ -49,6 +49,9 @@ public class TestJUnitPlaygroundOwner {
         p1.setName("CampoSP");
         p1.setOwner("Ricardo");
         p1.setCancellationPeriod(10);
+        
+        systemIn.provideLines("50");
+        playground.setPrice();
 
         bal = new eWallet();
         bal.setBalance(1000);
@@ -117,6 +120,17 @@ public class TestJUnitPlaygroundOwner {
     }
 
     @Test
+    public void testeAtualizaPlaygroundStatusInvalido()
+    {
+        owner.addPlayground(p1);
+
+        systemIn.provideLines("4","1notavailable","not available");
+        owner.updatePlaygroundName("CampoSP");
+        
+        assertEquals("not available",p1.getStatus());
+    }
+
+    @Test
     public void testeAtualizaPlaygroundBookTime()
     {
         owner.addPlayground(p1);
@@ -127,6 +141,18 @@ public class TestJUnitPlaygroundOwner {
         assertEquals(10,p1.getSlotsBegin());
         assertEquals(22,p1.getSlotsEnd());
     }
+
+    @Test
+    public void testeAtualizaPlaygroundIDInvalido()
+    {
+        owner.addPlayground(p1);
+
+        systemIn.provideLines("6","1","NovoCampoSP");
+        owner.updatePlaygroundName("CampoSP");
+        
+        assertTrue(owner.existPlayground("NovoCampoSP"));
+    }
+
 
     @Test
     public void testeAdicionaBalance()
@@ -150,5 +176,14 @@ public class TestJUnitPlaygroundOwner {
         owner.displayRecieveMsg();
 
         assertEquals("Olha playground",systemOutRule.getLog().trim());
+    }
+
+    @Test
+    public void testePagamento()
+    {
+        owner.addPlayground(p1);
+
+        owner.payMoney("CampoSP", 3);
+        assertEquals(850,owner.getMyBalance());
     }
 }
